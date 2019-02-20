242c1644924206001ab11901d75a7ace9815fddf
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 20:26:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
     VALUES (?, ?) 
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    
+    @id = 
     
     
   # Remember, you can access your database connection anywhere in this class
