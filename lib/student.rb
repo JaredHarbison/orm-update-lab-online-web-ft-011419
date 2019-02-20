176341481fb925bcb30fdb5639936e425d338e66
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 20:25:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,9 @@ class Student
   end 
   
   def save 
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
