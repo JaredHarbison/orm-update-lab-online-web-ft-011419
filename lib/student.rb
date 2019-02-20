569c481576c8a4a719e2e049084da434ed7861d6
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 20:14:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_reader :id 
+  attr_accessor :name, :grade 
+
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
